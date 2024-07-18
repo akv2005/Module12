e0e82294ab26efbc8524deb7f6015fcc04d18a3d
akv05 <akv0571@gmail.com>
2024-07-18 22:18:10 +0900

    Домашнее задание по теме "Простые Юнит-Тесты"

---
+++ tests_12_1.py
@@ -0,0 +1,26 @@
+import unittest
+import runner
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        ocr = runner.Runner('Вася')
+        for _ in range(10):
+            ocr.walk()
+        self.assertEqual(ocr.distance, 50)
+
+    def test_run(self):
+        ocr = runner.Runner('Вася')
+        for _ in range(10):
+            ocr.run()
+        self.assertEqual(ocr.distance, 100)
+
+    def test_challenge(self):
+        Vasya = runner.Runner('Вася')
+        Olya = runner.Runner('Оля')
+        for _ in range(10):
+            Vasya .walk()
+            Olya.run()
+        self.assertNotEqual(Vasya.distance, Olya.distance)
+
+if __name__ == "__main":
+    unittest.main
